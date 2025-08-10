c5c9dbb20ed4457733646c2999c3414f4bc9887b
Aqilqarayev <152633557+Aqilqarayev@users.noreply.github.com>
2025-08-10 23:36:42 +0400

    Delete PrintlnStringVariable.java

--- PrintlnStringVariable.java
+++
@@ -1,6 +0,0 @@
-public class PrintStringVariable {
-    public static void main(String[] args) {
-        String text = "Holberton School";
-        System.out.println(text + "\n" + text+ "\n"+ text.substring(0,9));
-    }
-}
