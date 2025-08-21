1399703e717e56807b456791f1b60cd4552398ad
Aqilqarayev <152633557+Aqilqarayev@users.noreply.github.com>
2025-08-22 00:02:21 +0400

    commit

---
+++ src/Character.java
@@ -0,0 +1,6 @@
+public class Character {
+    public static boolean isUpper(char c) {
+           if(c > 'A' && c < 'Z') return true;
+           else return false;
+    }
+}
---
+++ src/Program.java
@@ -0,0 +1,15 @@
+public class Program {
+    public static void main(String[] args) {
+        boolean characterInUppercase1 = Character.isUpper('a');
+        boolean characterInUppercase2 = Character.isUpper('B');
+        boolean characterInUppercase3 = Character.isUpper('d');
+        boolean characterInUppercase4 = Character.isUpper('f');
+        boolean characterInUppercase5 = Character.isUpper('G');
+
+        System.out.printf("%b\n", characterInUppercase1);
+        System.out.printf("%b\n", characterInUppercase2);
+        System.out.printf("%b\n", characterInUppercase3);
+        System.out.printf("%b\n", characterInUppercase4);
+        System.out.printf("%b\n", characterInUppercase5);
+    }
+}
