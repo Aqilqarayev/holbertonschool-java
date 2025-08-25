9f7ef3a233a6f73b7802fe81f08a63fc9418adec
Aqilqarayev <152633557+Aqilqarayev@users.noreply.github.com>
2025-08-25 21:29:24 +0400

    commit

--- 0x01/numbers/Numbers0to99.java
+++ 0x01/numbers/Numbers0to99.java
@@ -1,10 +1,10 @@
 
-public class NumbersOto99 {
+public class Numbers0to99 {
     public static void main(String[] args) {
         for (int i = 0; i < 100; ++i) {
-            if(i<99){
-            System.out.print( i + ", ");
+            if (i < 99) {
+                System.out.print(i + ", ");
+            } else System.out.println(i);
         }
-            else System.out.println(i);
     }
 }
